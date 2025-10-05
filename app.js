a1c7ab8f1e760173c2361fcf014b343a17bc5da0
Steffen Bruchmann <github@sbruchmann.me>
2025-10-05 03:08:13 +0200

    Render random phrase per request

--- app.js
+++ app.js
@@ -5,6 +5,31 @@ import favicon from "express-favicon";
 import { dirname as getDirectoryName, resolve as resolvePath } from "node:path";
 import { fileURLToPath } from "node:url";
 import helmet from "helmet";
+import { readFileSync } from "node:fs";
+import phrases from "./data/phrases.json" with { type: "json" };
+
+function randomRange(min, max) {
+	return Math.random() * (max - min) + min;
+}
+
+function sample(array) {
+	return array[Math.round(randomRange(0, array.length - 1))];
+}
+
+function makeSampler(array) {
+	let pool = [];
+
+	function refill() {
+		pool = [...array].sort(() => Math.random() - 0.5);
+	}
+
+	refill();
+
+	return function take() {
+		if (pool.length === 0) refill();
+		return pool.pop();
+	};
+}
 
 process.on("uncaughtException", (err) => {
 	console.error(err);
@@ -19,6 +44,18 @@ process.on("unhandledRejection", (err) => {
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = getDirectoryName(__filename);
 
+const indexTemplate = readFileSync("./views/index.html", "utf8");
+const getPhrase = makeSampler(phrases);
+function index(req, res, next) {
+	const indexBody = indexTemplate.replace(/\{\{phrase\}\}/, getPhrase());
+
+	res.writeHead(200, {
+		"Content-Length": Buffer.byteLength(indexBody),
+		"Content-Type": "text/html",
+	});
+	res.end(indexBody);
+}
+
 const app = express();
 app.use(compression());
 app.use(
@@ -28,9 +65,12 @@ app.use(
 			"script-src": ["'self'", "sbruchmann.me"],
 			"style-src": null,
 		},
-	})
+	}),
 );
+
 app.use(favicon(resolvePath(__dirname, "public", "favicon.png")));
+app.get("/", index);
+
 app.use(express.static(resolvePath(__dirname, "public")));
 
 const port = process.env.PORT || 3000;
